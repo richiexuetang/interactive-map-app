5713702e309da7c2e8f58937ee7e1de9e01fd7f7
richiexuetang <richard772794659@gmail.com>
2023-05-20 12:56:50 -0700

    toast css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,27 +2,31 @@ import "@styles/globals.scss";
 import theme from "@styles/theme";
 import { ChakraProvider } from "@chakra-ui/react";
 import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { MapProvider } from "../context/app-context";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <MapProvider>
-      <ChakraProvider theme={theme}>
-        <ToastContainer
-          position="top-right"
-          autoClose={500}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="dark"
-        />
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </MapProvider>
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+
+      <MapProvider>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </MapProvider>
+    </>
   );
 }
 
