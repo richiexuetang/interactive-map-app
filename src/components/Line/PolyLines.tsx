9d8ccd03f67da0eb8dd0f2a5ef3e007c338494c9
richiexuetang <richard772794659@gmail.com>
2023-06-16 19:20:18 -0700

    add categories

--- src/components/Line/PolyLines.tsx
+++ src/components/Line/PolyLines.tsx
@@ -1,20 +1,20 @@
 import { useLocalStorage } from "@hooks/index";
 import Polyline from "./PolyLine";
 
-import { useMapContext } from "@context/app-context";
-import { COMPLETED } from "@data/LocalStorage";
+import { COMPLETED, USER_SETTING, initialUserSettings } from "@data/LocalStorage";
 import { categoryHiddenState } from "@lib/getHiddenState";
 
 const PolyLines = (props) => {
   const { pathMarkers } = props;
   const [completedMarkers] = useLocalStorage(COMPLETED, {});
+  const [userSettings] = useLocalStorage(USER_SETTING, initialUserSettings)
 
   return (
     <>
       {pathMarkers &&
         pathMarkers.map(({ parentId, path }, i) => {
           return (
-            !completedMarkers[parentId] && !categoryHiddenState(completedMarkers[parentId]) && (
+            !completedMarkers[parentId] && !categoryHiddenState(89) && (
               <Polyline key={`${parentId} + ${i}`} path={path} />
             )
           );
